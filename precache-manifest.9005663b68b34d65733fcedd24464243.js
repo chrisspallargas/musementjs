e6f522bdf2f58783cbd84269aa49ac5af9c9a110
Chrisspallargas <chrisspallargas@gmail.com>
2019-10-24 19:02:54 +0200

    new build

---
+++ precache-manifest.9005663b68b34d65733fcedd24464243.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec549d82425791148a45104b8258dc6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e437e0b363db5c2b82b",
+    "url": "./static/css/main.6cf6cf6f.chunk.css"
+  },
+  {
+    "revision": "cfdecb65ad86309d206e",
+    "url": "./static/js/2.ac0ac14c.chunk.js"
+  },
+  {
+    "revision": "5e437e0b363db5c2b82b",
+    "url": "./static/js/main.e5f3c32d.chunk.js"
+  },
+  {
+    "revision": "1a04ff9921af53ff26ef",
+    "url": "./static/js/runtime-main.a346b55e.js"
+  },
+  {
+    "revision": "44dfe8cc676882243911a3197a50169e",
+    "url": "./static/media/Lato-Bold.44dfe8cc.ttf"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "./static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "549a8579f28db958753394b84c7dea64",
+    "url": "./static/media/arrow-left.549a8579.svg"
+  },
+  {
+    "revision": "5d1fa2b8f3a25f42dde4e4694e7dfaba",
+    "url": "./static/media/arrow-right.5d1fa2b8.svg"
+  },
+  {
+    "revision": "abb11376e8690e2f93299e2e16d7a2f7",
+    "url": "./static/media/bag.abb11376.svg"
+  },
+  {
+    "revision": "0bb927b777b03834fcd45041f49f6dcd",
+    "url": "./static/media/wishlist.0bb927b7.svg"
+  }
+]);
